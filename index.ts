9bf5439806cfbaff43b8f74716a4272f09047e25
oda <oda.mikio@gmail.com>
2016-05-19 13:46:34 +0900

    first commit
    angular material on electron

---
+++ index.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by oda on 2016/05/19.
+ */
+
+/// <reference path="typings/main.d.ts" />
+
+"use strict";
+
+const BrowserWindow = require('electron').remote.BrowserWindow;
+const path = require('path');
+
+var app:any = angular.module('Application', ["ngResource", 'ngMessages', 'ngAnimate', 'ngMaterial', 'ngMdIcons']);
+
+app.controller('Controller', ["$scope",
+    ($scope:any):void => {
+        
+        $scope.modal = () => {
+            const modalPath = path.join('file://', __dirname, 'modal.html');
+            let win = new BrowserWindow({ width: 400, height: 320 });
+            win.on('closed', function () { win = null });
+            win.loadURL(modalPath);
+            win.show();
+        }
+        
+    }]);
