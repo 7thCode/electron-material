d441f2445f864f61cc0b2d72f25af4c626339fe6
oda <oda.mikio@gmail.com>
2016-05-19 15:36:01 +0900

    ...

---
+++ modal.js
@@ -0,0 +1,20 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+/// <reference path="typings/main.d.ts" />
+"use strict";
+/*
+const BrowserWindow = require('electron').remote.BrowserWindow;
+const path = require('path');
+
+const newWindowBtn = document.getElementById('new-window');
+
+newWindowBtn.addEventListener('click', function (event) {
+    const modalPath = path.join('file://', __dirname, 'modal.html');
+    let win = new BrowserWindow({ width: 400, height: 320 });
+    win.on('closed', function () { win = null });
+    win.loadURL(modalPath);
+    win.show();
+});
+*/
+//# sourceMappingURL=modal.js.map
